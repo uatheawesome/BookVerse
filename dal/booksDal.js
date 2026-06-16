5d328312b7b1401b64089a5d5ac5f38951372302
ua <uawesome64@gmail.com>
2026-06-16 19:49:15 +0300

    skelly done started booksdal database

---
+++ dal/booksDal.js
@@ -0,0 +1,16 @@
+const Local_Library = [];
+
+function getAllBooks() { }
+function getBookById(id) { }
+function addBook(book) { }
+function removeBook(id) { }
+function updateBook(id, book) { }
+//function initializeLibrary() { } i may wanna add
+
+module.exports = {
+    getAllBooks,
+    getBookById,
+    addBook,
+    removeBook,
+    updateBook
+};
